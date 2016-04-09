ac3c7fa538efe5855430ed0439c13881faf93db8
xmolna02 <xmolna02@stud.fit.vutbr.cz>
2016-04-09 12:24:14 +0200

    VersionAttribute

---
+++ src/Nyx/Nyx.Foundation/AppVersionInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Nyx.Foundation.Attributes;
+
+namespace Nyx.Foundation
+{
+    public class AppVersionInfo
+    {
+        public string GetCurrentVersion()
+        {
+            var assembly = AppDomain.CurrentDomain.GetAssemblies().FirstOrDefault(a => a.GetCustomAttribute<AppVersionAssembly>() != null);
+            var attr = assembly?.GetCustomAttributes<AssemblyInformationalVersionAttribute>().FirstOrDefault();
+            return attr?.InformationalVersion;
+        }
+    }
+}
---
+++ src/Nyx/Nyx.Foundation/Attributes/AppVersionAssembly.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nyx.Foundation.Attributes
+{
+    [AttributeUsage(AttributeTargets.Assembly, AllowMultiple = false)]
+    public class AppVersionAssembly : Attribute
+    {
+
+    }
+}
