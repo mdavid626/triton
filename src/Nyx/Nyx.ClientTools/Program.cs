9af40f4c80dcff1c0b78307921c613c493ae97b8
xmolna02 <xmolna02@stud.fit.vutbr.cz>
2016-04-09 15:09:09 +0200

    ClientTools - message

--- src/Nyx/Nyx.ClientTools/Program.cs
+++ src/Nyx/Nyx.ClientTools/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Nyx.ClientTools
 {
@@ -10,6 +11,10 @@ namespace Nyx.ClientTools
     {
         static void Main(string[] args)
         {
+            var msg = "Message from Nyx.ClientTools";
+            if (args.Any())
+                msg += ": " + String.Join(", ", args);
+            MessageBox.Show(msg, "Nyx", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
     }
 }
