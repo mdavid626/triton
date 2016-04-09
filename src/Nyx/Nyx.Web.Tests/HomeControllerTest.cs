2eefcf81e158d272a3f38965eaf9ac1fa3fabf21
xmolna02 <xmolna02@stud.fit.vutbr.cz>
2016-04-09 13:26:13 +0200

    Unit testy

---
+++ src/Nyx/Nyx.Web.Tests/HomeControllerTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Nyx.Web.Controllers;
+
+namespace Nyx.Web.Tests
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        [TestMethod]
+        public void IndexTest()
+        {
+            var ctrl = new HomeController();
+            var result = ctrl.Index() as ViewResult;
+            Assert.AreEqual("", result.ViewName);
+        }
+    }
+}
