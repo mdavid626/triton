cd6958081821b647f2bebcb5ba542918b16ec875
xmolna02 <xmolna02@stud.fit.vutbr.cz>
2016-04-09 16:56:07 +0200

    ClientTools - version info

--- src/Nyx/Nyx.ClientTools/Program.cs
+++ src/Nyx/Nyx.ClientTools/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Nyx.Foundation;
 
 namespace Nyx.ClientTools
 {
@@ -11,9 +12,11 @@ namespace Nyx.ClientTools
     {
         static void Main(string[] args)
         {
+            var version = new AppVersionInfo().GetCurrentVersion();
             var msg = "Message from Nyx.ClientTools";
             if (args.Any())
                 msg += ": " + String.Join(", ", args);
+            msg += "\nVersion: " + version;
             MessageBox.Show(msg, "Nyx", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
     }
