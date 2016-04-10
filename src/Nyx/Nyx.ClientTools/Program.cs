ef2aae583db07b6faafb234559c7a65dfa307175
xmolna02 <xmolna02@stud.fit.vutbr.cz>
2016-04-10 12:20:45 +0200

    zavislost Cadmus.Foundation, Cadmus.DbUp

--- src/Nyx/Nyx.ClientTools/Program.cs
+++ src/Nyx/Nyx.ClientTools/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using Nyx.Foundation;
+using Cadmus.Foundation;
 
 namespace Nyx.ClientTools
 {
--- src/Nyx/Nyx.Web/Properties/AssemblyInfo.cs
+++ src/Nyx/Nyx.Web/Properties/AssemblyInfo.cs
@@ -1,7 +1,7 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-using Nyx.Foundation.Attributes;
+using Cadmus.Foundation.Metadata;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
