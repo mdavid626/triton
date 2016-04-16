e673958976b78d2e2f3345515c4f32f8bd355893
xmolna02 <xmolna02@stud.fit.vutbr.cz>
2016-04-16 17:26:06 +0200

    Hector Tool Window

---
+++ src/Tools/Hector/Hector/ToolWindows/HectorToolWindow.cs
@@ -0,0 +1,40 @@
+﻿//------------------------------------------------------------------------------
+// <copyright file="HectorToolWindow.cs" company="Company">
+//     Copyright (c) Company.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+
+namespace Hector
+{
+    using System;
+    using System.Runtime.InteropServices;
+    using Microsoft.VisualStudio.Shell;
+
+    /// <summary>
+    /// This class implements the tool window exposed by this package and hosts a user control.
+    /// </summary>
+    /// <remarks>
+    /// In Visual Studio tool windows are composed of a frame (implemented by the shell) and a pane,
+    /// usually implemented by the package implementer.
+    /// <para>
+    /// This class derives from the ToolWindowPane class provided from the MPF in order to use its
+    /// implementation of the IVsUIElementPane interface.
+    /// </para>
+    /// </remarks>
+    [Guid("5d750d2d-b420-4588-b370-f9597b9f6e83")]
+    public class HectorToolWindow : ToolWindowPane
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="HectorToolWindow"/> class.
+        /// </summary>
+        public HectorToolWindow() : base(null)
+        {
+            this.Caption = "Hector Tools";
+
+            // This is the user control hosted by the tool window; Note that, even if this class implements IDisposable,
+            // we are not calling Dispose on this object. This is because ToolWindowPane calls Dispose on
+            // the object returned by the Content property.
+            this.Content = new HectorToolWindowControl();
+        }
+    }
+}
---
+++ src/Tools/Hector/Hector/View/HectorToolWindowControl.xaml.cs
@@ -0,0 +1,40 @@
+﻿//------------------------------------------------------------------------------
+// <copyright file="HectorToolWindowControl.xaml.cs" company="Company">
+//     Copyright (c) Company.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+
+namespace Hector
+{
+    using System.Diagnostics.CodeAnalysis;
+    using System.Windows;
+    using System.Windows.Controls;
+
+    /// <summary>
+    /// Interaction logic for HectorToolWindowControl.
+    /// </summary>
+    public partial class HectorToolWindowControl : UserControl
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="HectorToolWindowControl"/> class.
+        /// </summary>
+        public HectorToolWindowControl()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Handles click on the button by displaying a message box.
+        /// </summary>
+        /// <param name="sender">The event sender.</param>
+        /// <param name="e">The event args.</param>
+        [SuppressMessage("Microsoft.Globalization", "CA1300:SpecifyMessageBoxOptions", Justification = "Sample code")]
+        [SuppressMessage("StyleCop.CSharp.NamingRules", "SA1300:ElementMustBeginWithUpperCaseLetter", Justification = "Default event handler naming pattern")]
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(
+                string.Format(System.Globalization.CultureInfo.CurrentUICulture, "Invoked '{0}'", this.ToString()),
+                "HectorToolWindow");
+        }
+    }
+}
