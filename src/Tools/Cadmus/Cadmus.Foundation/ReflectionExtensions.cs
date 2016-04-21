346a1ac3e2dac424fd6e8d250220a365e414d8e9
xmolna02 <xmolna02@stud.fit.vutbr.cz>
2016-04-21 20:32:50 +0200

    ReflectionExtensions

---
+++ src/Tools/Cadmus/Cadmus.Foundation/ReflectionExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cadmus.Foundation
+{
+    public static class ReflectionExtensions
+    {
+        public static string ToSha256(this string text)
+        {
+            var bytes = Encoding.Unicode.GetBytes(text);
+            var hashstring = new SHA256Managed();
+            var hash = hashstring.ComputeHash(bytes);
+            var hashString = string.Empty;
+            foreach (var x in hash)
+            {
+                hashString += $"{x:x2}";
+            }
+            return hashString;
+        }
+    }
+}
