8e548fbffe1a797db8ffab05f26bd272971b3a9c
xmolna02 <xmolna02@stud.fit.vutbr.cz>
2016-04-23 14:16:49 +0200

    Test Data

---
+++ src/Nyx/Nyx.DbProject/dbo/PostDeployment/InsertTestData.sql
@@ -0,0 +1,31 @@
+﻿insert into [Transaction] (Amount, [From], [To], Purpose, Category, SubCategory, Note, Created)
+values (100, 'Bank', 'ŠKODA AUTO a.s.', 'fakturace ', 'auto', 'pohonne hmoty', null, getdate())
+
+insert into [Transaction] (Amount, [From], [To], Purpose, Category, SubCategory, Note, Created)
+values (200, 'Bank', 'ČEZ, a. s.', 'fakturace', 'energie', null, null, getdate())
+
+insert into [Transaction] (Amount, [From], [To], Purpose, Category, SubCategory, Note, Created)
+values (300, 'Pokladna', 'RWE Supply & Trading CZ, a.s.', 'fakturace', 'potraviny', null, null, getdate())
+
+insert into [Transaction] (Amount, [From], [To], Purpose, Category, SubCategory, Note, Created)
+values (400, 'Pokladna', 'UNIPETROL, a.s.', 'fakturace', 'cestovani', null, null, getdate())
+
+insert into [Transaction] (Amount, [From], [To], Purpose, Category, SubCategory, Note, Created)
+values (500, 'Pokladna', 'FOXCONN CZ s.r.o.', 'fakturace', 'energie', null, null, getdate())
+
+insert into [Transaction] (Amount, [From], [To], Purpose, Category, SubCategory, Note, Created)
+values (600, 'Bank', 'ČEPRO, a.s.', 'fakturace', 'energie', null, null, getdate())
+
+insert into [Transaction] (Amount, [From], [To], Purpose, Category, SubCategory, Note, Created)
+values (700, 'Pokladna', 'E.ON Energie, a.s.', 'fakturace', 'energie', null, null, getdate())
+
+insert into [Transaction] (Amount, [From], [To], Purpose, Category, SubCategory, Note, Created)
+values (800, 'Pokladna', 'Tesco Stores ČR a.s.', 'fakturace', 'cestovani', null, null, getdate())
+
+insert into [Transaction] (Amount, [From], [To], Purpose, Category, SubCategory, Note, Created)
+values (900, 'Pokladna', 'Česká spořitelna, a.s.', 'fakturace', 'cestovani', null, null, getdate())
+
+insert into [Transaction] (Amount, [From], [To], Purpose, Category, SubCategory, Note, Created)
+values (1100, 'Pokladna', 'RWE Energie, a.s.', 'fakturace', 'potraviny', null, null, getdate())
+
+
