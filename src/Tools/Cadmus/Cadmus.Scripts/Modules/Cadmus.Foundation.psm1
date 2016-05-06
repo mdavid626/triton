0b01addc506482fe8c70c9f7f2fa9e7fc677e775
xmolna02 <xmolna02@stud.fit.vutbr.cz>
2016-05-06 12:26:21 +0200

    ColoredInfo refaktoring

--- src/Tools/Cadmus/Cadmus.Scripts/Modules/Cadmus.Foundation.psm1
+++ src/Tools/Cadmus/Cadmus.Scripts/Modules/Cadmus.Foundation.psm1
@@ -2,46 +2,35 @@
 # Cadmus.Foundation.psm1
 #
 
-function Write-Success()
+function Write-ColoredInfo()
 {
-	param ([string] $Message)
-
+	param ([string] $Message, [ConsoleColor] $Color, [string] $RedirectColor)
 	if ([Console]::IsOutputRedirected) 
 	{
-		Write-Host "{Success}$Message"
+		Write-Host "{$RedirectColor}$Message"
 	} 
 	else
 	{
-		Write-Host $Message -ForegroundColor Green
+		Write-Host $Message -ForegroundColor $Color
 	}
+}
+
+function Write-Success()
+{
+	param ([string] $Message)
+	Write-ColoredInfo -Message $Message -Color Green -RedirectColor "Success"
 }
 
 function Write-Header()
 {
 	param ([string] $Message)
-
-	if ([Console]::IsOutputRedirected) 
-	{
-		Write-Host "{Header}$Message"
-	} 
-	else
-	{
-		Write-Host $Message -ForegroundColor Cyan
-	}
+	Write-ColoredInfo -Message Cyan -Color Green -RedirectColor "Header"
 }
 
 function Write-Warn()
 {
 	param ([string] $Message)
-
-	if ([Console]::IsOutputRedirected) 
-	{
-		Write-Host "{Warning}$Message"
-	} 
-	else
-	{
-		Write-Host $Message -ForegroundColor Yellow
-	}
+	Write-ColoredInfo -Message $Message -Color Yellow -RedirectColor "Warning"
 }
 
 #Set-Item -Path WSMan:localhostClientTrustedHosts -Value ''
