422f6d5791aef01cdf5e474156a9e5675e4807d9
xmolna02 <xmolna02@stud.fit.vutbr.cz>
2016-05-06 17:42:23 +0200

    aktualizace nuget balicku

--- src/Nyx/Nyx.Installer/Modules/Cadmus.Foundation.psm1
+++ src/Nyx/Nyx.Installer/Modules/Cadmus.Foundation.psm1
@@ -1,3 +1,55 @@
 #
-# Cadmus.psm1
+# Cadmus.Foundation.psm1
 #
+
+Add-Type -Path 'Cadmus.Foundation.dll'
+$logger = New-Object -TypeName 'Cadmus.Foundation.ConsoleLogger'
+
+function Log-Info() 
+{
+	param ([string] $Message)
+	$logger.LogInfo($Message)
+}
+
+function Log-Success() 
+{
+	param ([string] $Message)
+	$logger.LogSuccess($Message)
+}
+
+function Log-Warning() 
+{
+	param ([string] $Message)
+	$logger.LogWarning($Message)
+}
+
+function Log-Error() 
+{
+	param ([string] $Message)
+	$logger.LogError($Message)
+}
+
+function Log-Verbose()
+{
+	param ([string] $Message)
+	$logger.LogVerbose($Message)
+}
+
+function Log-Header() 
+{
+	param ([string] $Message)
+	$logger.LogHeader($Message)
+}
+
+function Start-Verbose()
+{
+	$logger.StartVerbose()
+}
+
+function Stop-Verbose()
+{
+	$logger.StopVerbose()
+}
+
+
+#Set-Item -Path WSMan:localhostClientTrustedHosts -Value ''
--- src/Tools/Cadmus/Cadmus.VisualFoundation/Properties/AssemblyInfo.cs
+++ src/Tools/Cadmus/Cadmus.VisualFoundation/Properties/AssemblyInfo.cs
@@ -33,9 +33,9 @@ using System.Windows.Markup;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.2.0")]
+[assembly: AssemblyFileVersion("1.0.2.0")]
+[assembly: AssemblyInformationalVersion("1.0.2.0")]
 
 [assembly: XmlnsDefinition("http://schemas.cymric.me/vf", "Cadmus.VisualFoundation.Framework")]
 [assembly: XmlnsDefinition("http://schemas.cymric.me/vf", "Cadmus.VisualFoundation.Controls")]
