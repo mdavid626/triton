494b49cd8ca277ce314f2f598c43a8882d90d06e
xmolna02 <xmolna02@stud.fit.vutbr.cz>
2016-05-09 21:24:47 +0200

    web deploy install

--- src/Tools/Cadmus/Cadmus.Scripts/Cookbooks/cadmus/recipes/web.rb
+++ src/Tools/Cadmus/Cadmus.Scripts/Cookbooks/cadmus/recipes/web.rb
@@ -20,3 +20,9 @@ windows_feature 'IIS-ASPNET45' do
   action :install
   all true
 end
+
+# Web Deploy
+windows_package 'Web Deploy 3.6' do
+  source 'http://10.0.0.1:8080/WebDeploy/WebDeploy_amd64_en-US.msi'
+  action :install
+end
