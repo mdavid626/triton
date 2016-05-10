a4e0e06516f00489b3fd8f3c682d2585c68d37ba
xmolna02 <xmolna02@stud.fit.vutbr.cz>
2016-05-10 10:51:11 +0200

    refaktoring

--- src/Tools/Cadmus/Cadmus.Scripts/Cookbooks/cadmus/recipes/web.rb
+++ src/Tools/Cadmus/Cadmus.Scripts/Cookbooks/cadmus/recipes/web.rb
@@ -1,6 +1,6 @@
 #
 # Cookbook Name:: cadmus
-# Recipe:: default
+# Recipe:: web
 #
 # Copyright 2016, Cymric
 #
@@ -23,23 +23,6 @@ end
 windows_package 'Web Deploy' do
   source node['cadmus']['webdeploy']['url']
   action :install
-end
-
-# WMF 5.0
-powershell_script 'install-wmf5' do
-  code <<-EOH
-  $hotfix = Get-HotFix -Id KB3134758
-  if (-Not $hotfix) {
-      wget http://10.0.0.1:8080/MSU/Win8.1AndW2K12R2-KB3134758-x64.msu -OutFile package.msu
-      $path = (Get-ChildItem package.msu).FullName
-      $args = @('/install', "$path", '/quiet', '/norestart')   
-      $code = Start-Process wusa.exe -NoNewWindow -Wait -ArgumentList $args -PassThru -ErrorAction Stop
-	  Remove-Item -Path package.msu -Force
-      if ($code.ExitCode -ne 3010) {
-          throw "Instalation failed"   
-      }
-  }
-  EOH
 end
 
 # .NET Framework
